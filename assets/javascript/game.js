d6d276d0ea0eaa2b6014e709637abedb35fb3c4b
Rubi Renteria <rubirenteria43@gmail.com>
2019-09-12 17:41:13 -0600

    just finished designing the layout of the game. About to add JS.

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+
+//on CLick Events
+$(document).ready(function(){
+    $("#gem").on('click',function(){
+        oof.play();
+
+    })
+})
+$(document).ready(function(){
+    $("#white").on('click',function(){
+        oof.play();
+
+    })
+})
+$(document).ready(function(){
+    $("#blue").on('click',function(){
+        oof.play(); 
+             
+
+    })
+})
+$(document).ready(function(){
+    $("#purple").on('click',function(){
+        oof.play();
+
+    })
+})
+
+//audio
+var oof = new Audio();
+oof.src = "roblox.mp3";
+
+//
